0fe672a087edc0a4863c0b003d11f8abf249aef1
George A. Kotch <2021154499@ub.edu.bz>
2024-10-16 12:28:33 -0600

    Database setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const appVersion = "1.0.0"
@@ -14,6 +18,9 @@ const appVersion = "1.0.0"
 type serverConfig struct {
 	port        int
 	environment string
+	db          struct {
+		dsn string
+	}
 }
 
 type applicationDependecies struct {
@@ -28,9 +35,23 @@ func main() {
 	flag.IntVar(&settings.port, "port", 4000, "Server port")
 	flag.StringVar(&settings.environment, "env", "development", "Environment(developmnet|staging|production)")
 
+	flag.StringVar(&settings.db.dsn, "db-dsn", "postgres://comments:fishsticks@localhost/comments?sslmode=disable", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// the call to openDB() sets up our connection pool
+	db, err := openDB(settings)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// release the database resources before exiting
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	appInstance := &applicationDependecies{
 		config: settings,
@@ -49,8 +70,33 @@ func main() {
 	}
 
 	logger.Info("starting server", "address", apiServer.Addr, "environment", settings.environment)
-	err := apiServer.ListenAndServe()
+	err = apiServer.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+
+}
+
+func openDB(settings serverConfig) (*sql.DB, error) {
+	// open a connection pool
+	db, err := sql.Open("postgres", settings.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// set a context to ensure DB operations don't take too long
+	ctx, cancel := context.WithTimeout(context.Background(),
+		5*time.Second)
+	defer cancel()
+
+	// let's test if the connection pool was created
+	// we trying pinging it with a 5-second timeout
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	// return the connection pool (sql.DB)
+	return db, nil
 
 }
