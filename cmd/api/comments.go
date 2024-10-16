42298bbf17c6b9e48524fe9b194a83abefe9b87d
George A. Kotch <2021154499@ub.edu.bz>
2024-10-15 22:44:33 -0600

    parse-json-v2

--- cmd/api/comments.go
+++ cmd/api/comments.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"net/http"
 	// import the data package which contains the definition for Comment
@@ -17,9 +17,10 @@ func (a *applicationDependecies) createCommentHandler(w http.ResponseWriter,
 		Author  string `json:"author"`
 	}
 	// perform the decoding
-	err := json.NewDecoder(r.Body).Decode(&incomingData)
+	// err := json.NewDecoder(r.Body).Decode(&incomingData)
+	err := a.readJSON(w, r, &incomingData)
 	if err != nil {
-		a.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
+		a.badRequestResponse(w, r, err)
 		return
 	}
 
--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"fmt"
@@ -6,28 +6,27 @@ import (
 )
 
 // log an error message
-func (a *applicationDependecies)logError(r *http.Request, err error)  {
+func (a *applicationDependecies) logError(r *http.Request, err error) {
 
 	method := r.Method
 	uri := r.URL.RequestURI()
 	a.logger.Error(err.Error(), "method", method, "uri", uri)
-	 
- }
- 
+
+}
+
 // send an error response in JSON
-func (a *applicationDependecies)errorResponseJSON(w http.ResponseWriter,r *http.Request, status int, message any)  {
+func (a *applicationDependecies) errorResponseJSON(w http.ResponseWriter, r *http.Request, status int, message any) {
 
 	errorData := envelope{"error": message}
 	err := a.writeJSON(w, status, errorData, nil)
 	if err != nil {
 		a.logError(r, err)
 		w.WriteHeader(500)
-	}   
+	}
 }
 
-
 // send an error response if our server messes up
-func (a *applicationDependecies)serverErrorResponse(w http.ResponseWriter,r *http.Request, err error)  {
+func (a *applicationDependecies) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 
 	// first thing is to log error message
 	a.logError(r, err)
@@ -36,27 +35,30 @@ func (a *applicationDependecies)serverErrorResponse(w http.ResponseWriter,r *htt
 	a.errorResponseJSON(w, r, http.StatusInternalServerError, message)
 }
 
-
 // send an error response if our client messes up with a 404
-func (a *applicationDependecies)notFoundResponse(w http.ResponseWriter, r *http.Request)  {
-
-			// we only log server errors, not client errors
-			// prepare a response to send to the client
-			message := "the requested resource could not be found"
-			a.errorResponseJSON(w, r, http.StatusNotFound, message)
-}
-
-
-  //send an error response if our client messes up with a 405 
-  func (a *applicationDependecies)methodNotAllowedResponse(w http.ResponseWriter, r *http.Request)  {
+func (a *applicationDependecies) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 
 	// we only log server errors, not client errors
-	// prepare a formatted response to send to the client 
-	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method) 
+	// prepare a response to send to the client
+	message := "the requested resource could not be found"
+	a.errorResponseJSON(w, r, http.StatusNotFound, message)
+}
+
+// send an error response if our client messes up with a 405
+func (a *applicationDependecies) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+
+	// we only log server errors, not client errors
+	// prepare a formatted response to send to the client
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 
 	a.errorResponseJSON(w, r, http.StatusMethodNotAllowed, message)
 
-  }
+}
 
+// send an error response if our client messes up with a 400 (bad request)
+func (a *applicationDependecies) badRequestResponse(w http.ResponseWriter,
+	r *http.Request,
+	err error) {
 
-  
+	a.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
+}
